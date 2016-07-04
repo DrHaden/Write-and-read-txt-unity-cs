86f8bab1c81554a353fe64b7ee95c407a6fdb37b
DrHaden <hayden.a.schmidt@gmail.com>
2016-07-04 20:15:17 +1200

    Major Changes.
    I have reconfigures most of the write to file using two different
    methods of writing to the file. Another change was the fact the the
    "pos" array was split into multiple arrays allowing for storing all
    data. Public voids were added to be run by buttons in the unity game.
    Two things are now a problem including that when I add a new set it
    overwrites everything including the lognum line at the first line of the
    file and the last issue of issue of:
    
    ndexOutOfRangeException: Array index is out of
    range.
    Data_Receive_and_Display.loadset () (at
    Assets/Data_Receive_and_Display.cs:74)

--- Data_Receive_and_Display.cs
+++ Data_Receive_and_Display.cs
@@ -5,47 +5,120 @@ using System.IO;
 using UnityEngine.UI;
 using System.Text;
 
-public class Data_Receive_and_Display : MonoBehaviour {
-    string filepath;
-    string patientname = "Hayden";
-    string readdate;
+public class Data_Receive_and_Display : MonoBehaviour
+{
+    string[] lines;
+    string patientname = "My name";
+    string datapath;
 
-    int[] pos = {22,55,3};
-    int[] readpos = new int[3];
+    DateTime[] readdate;
+    DateTime currentdate = DateTime.Now;
+
+    int[] pos1;
+    int[] pos2;
+    int[] pos3;
     int lognum;
 
     public Text all;
-    
 
-    void Start ()
+
+
+
+    void Start()
     {
-        string datapath =  Application.dataPath + "/patientdata/" + patientname;
-        Debug.Log(datapath);
-        string[] lines = File.ReadAllLines(datapath);
-        Debug.Log (File.ReadAllText(datapath));
+
+        datapath = Application.dataPath + "/patientdata/" + patientname + ".txt";
+        if (File.Exists(datapath)) { }
+        else
+        {
+            Debug.Log("hahahahahahahahah");
+            string[] setup = new string[1];
+            setup[0] = "0";
+            File.WriteAllLines(datapath, setup);
+        }
+        //--------------------------------------------
+        lines = File.ReadAllLines(datapath);
         int.TryParse(lines[0], out lognum);
+        //--------------------------------------------
+        readdate = new DateTime[lognum];
+        pos1 = new int[lognum];
+        pos2 = new int[lognum];
+        pos3 = new int[lognum];
+    }
+
+
+
+
+    public void addset()
+    {
         lognum++;
         lines[0] = lognum.ToString();
-            lines[lognum * 4 - 3] = (DateTime.Now.ToString());
-            lines[lognum * 4 - 2] = (pos[0]).ToString();
-            lines[lognum * 4 - 2] = (pos[1]).ToString();
-            lines[lognum * 4] = (pos[2]).ToString();
         File.WriteAllLines(datapath, lines);
-        //______________________________________________
-        for (int m = lognum; m >= 1; m--)
+        using (StreamWriter file = new StreamWriter(datapath))
         {
-            readdate = (lines[m * 4 - 3].ToString());
-            int.TryParse(lines[m * 4 - 2] , out readpos[0]);
-            int.TryParse(lines[m * 4 - 1], out readpos[1]);
-            int.TryParse(lines[m * 4], out readpos[2]);
-            
-            Debug.Log("Lognum: " + lognum);
-            Debug.Log("date: " + readdate);
-            Debug.Log("pos1: " + readpos[0]);
-            Debug.Log("pos2: " + readpos[1]);
-            Debug.Log("pos3: " + readpos[2]);
-
+            System.Random rnd = new System.Random();
+            file.WriteLine(currentdate.ToString());
+            file.WriteLine(rnd.Next(1, 100));
+            file.WriteLine(rnd.Next(1, 100));
+            file.WriteLine(rnd.Next(1, 100));
         }
+    }
+
+
+
+
+    public void loadset()
+    {
+        int i = 0;
+        for (int m = 3; m < lognum * 4 - 3; m = m + 4)
+        {
+            DateTime.TryParse(lines[m], out readdate[i]);
+            i++;
+        }
+        i = 0;
+        //--------------------------------------------
+        for (int m = 3; m < lognum * 4 - 2; m = m + 4)
+        {
+
+            int.TryParse(lines[m], out pos1[i]);
+            i++;
+        }
+        i = 0;
+
+        //--------------------------------------------
+        for (int m = 3; m < lognum * 4 - 1; m = m + 4)
+        {
+
+            int.TryParse(lines[m], out pos2[i]);
+            i++;
+        }
+        i = 0;
+
+        //--------------------------------------------
+        for (int m = 3; m < lognum * 4; m = m + 4)
+        {
+
+            int.TryParse(lines[m], out pos3[i]);
+            i++;
+        }
+        //--------------------------------------------
+        Debug.Log("Lognum: " + lognum);
+        int log = this.lognum;
+        for (int x = 0; x < lognum; x++)
+        {
+            Debug.Log("Lognum " + x + ": " + log);
+            Debug.Log("date: " + x + ": " + readdate[i]);
+            Debug.Log("pos1: " + x + ": " + pos1[i]);
+            Debug.Log("pos2: " + x + ": " + pos2[i]);
+            Debug.Log("pos3: " + x + ": " + pos3[i]);
+            log--;
+        }
+    }
+
+
+
+    void Update()
+    {
         all.text = File.ReadAllText(datapath);
     }
 }
